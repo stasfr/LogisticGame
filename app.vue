17fe280f1a76f9c9021d064daa7a3fc4f0adcd23
stasfr <stas240600@yandex.ru>
2023-06-04 21:59:26 +0300

    project init

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <MainApp />
+  </div>
+</template>
---
+++ components/MainApp.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
+  >
+    Hello, world!
+  </div>
+</template>
+
+<script setup></script>
