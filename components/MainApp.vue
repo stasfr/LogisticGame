ae2ff0de7f6e47dad1bdba8352d9d7704b261832
stasfr <stas240600@yandex.ru>
2023-06-05 23:52:11 +0300

    base tab appearance

--- components/MainApp.vue
+++ components/MainApp.vue
@@ -1,9 +1,51 @@
 <template>
-  <div
-    class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
+  <svg
+    class="l-0 t-0 min-w-3168 min-h-2712 absolute block h-full w-full cursor-move select-none"
   >
-    Hello, world!
-  </div>
+    <!-- <Tab :options="testOptions" /> -->
+    <Tab v-for="test in generateTests()" :key="test.id" :options="test" />
+  </svg>
 </template>
 
-<script setup></script>
+<script setup>
+const generateTests = () => {
+  let tests = [
+    {
+      id: 1,
+      x: 100,
+      y: 100,
+      width: 200,
+      height: 100
+    }
+  ];
+
+  for (let i = 2; i < 5; i++) {
+    tests.push({
+      id: i,
+      x: tests[i - 2].x + 250,
+      y: 100,
+      width: 200,
+      height: 100 * i * 0.5 + 100
+    });
+  }
+
+  for (let i = 5; i < 9; i++) {
+    tests.push({
+      id: i,
+      x: tests[i - 5].x + 250,
+      y: 500,
+      width: 200,
+      height: 100 * i * 0.5 + 100
+    });
+  }
+
+  return tests;
+};
+
+const testOptions = {
+  x: 100,
+  y: 100,
+  width: 300,
+  height: 200
+};
+</script>
---
+++ components/tab.vue
@@ -0,0 +1,57 @@
+<template>
+  <g class="cursor-grab">
+    <!-- main background -->
+    <rect
+      :x="props.options.x"
+      :y="props.options.y"
+      :width="props.options.width"
+      :height="props.options.height"
+      rx="10"
+      class="fill-slate-300 stroke-slate-600 stroke-1 hover:stroke-slate-950"
+      @mousedown="moveTab"
+    />
+
+    <g class="fill-slate-500">
+      <!-- header top -->
+      <rect
+        :x="props.options.x"
+        :y="props.options.y"
+        :width="props.options.width"
+        :height="props.options.height * 0.2"
+        rx="10"
+      />
+
+      <!-- header bottom -->
+      <rect
+        :x="props.options.x"
+        :y="props.options.y + props.options.height * 0.1"
+        :width="props.options.width"
+        :height="props.options.height * 0.1"
+      />
+    </g>
+
+    <text
+      :x="props.options.x + props.options.width / 2"
+      :y="props.options.y + props.options.height / 10"
+      dominant-baseline="middle"
+      text-anchor="middle"
+      fill="white"
+      >{{ props.options.id }}. w:{{ props.options.width }}, h:{{
+        props.options.height
+      }}</text
+    >
+  </g>
+</template>
+
+<script setup>
+const props = defineProps({
+  options: {
+    type: Object,
+    require: true
+  }
+});
+
+const moveTab = event => {
+  console.log(event);
+};
+</script>
