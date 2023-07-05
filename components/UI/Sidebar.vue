97e6f76ee3aedd081f02e1bd34f8aa858a0a0783
stasfr <stas240600@yandex.ru>
2023-07-05 21:56:59 +0300

    sidebar improve

--- components/UI/Sidebar.vue
+++ components/UI/Sidebar.vue
@@ -1,25 +1,63 @@
 <template>
   <div class="flex">
     <div
-      class="fixed left-0 top-0 m-0 flex h-screen w-16 flex-col bg-slate-950 text-white"
+      class="fixed left-0 top-0 m-0 flex h-screen w-16 flex-col justify-between bg-slate-950 text-white"
     >
-      <UISidebarIcon>
-        <template v-slot:icon>
-          <OfficeBuildingIcon />
-        </template>
-        <template v-slot:tooltip> Build </template>
-      </UISidebarIcon>
+      <div>
+        <UISidebarIcon>
+          <template v-slot:icon>
+            <OfficeBuildingIcon />
+          </template>
+          <template v-slot:tooltip> Build </template>
+        </UISidebarIcon>
 
-      <UISidebarIcon>
-        <template v-slot:icon>
-          <CogIcon />
-        </template>
-        <template v-slot:tooltip> Settings </template>
-      </UISidebarIcon>
+        <UISidebarIcon>
+          <template v-slot:icon>
+            <BeakerIcon />
+          </template>
+          <template v-slot:tooltip> Research </template>
+        </UISidebarIcon>
+
+        <UISidebarIcon>
+          <template v-slot:icon>
+            <CalculatorIcon />
+          </template>
+          <template v-slot:tooltip> Calculate </template>
+        </UISidebarIcon>
+
+        <UISidebarIcon>
+          <template v-slot:icon>
+            <ChartBarIcon />
+          </template>
+          <template v-slot:tooltip> Reports </template>
+        </UISidebarIcon>
+      </div>
+
+      <div>
+        <UISidebarIcon>
+          <template v-slot:icon>
+            <UserIcon />
+          </template>
+          <template v-slot:tooltip> User </template>
+        </UISidebarIcon>
+        <UISidebarIcon>
+          <template v-slot:icon>
+            <CogIcon />
+          </template>
+          <template v-slot:tooltip> Settings </template>
+        </UISidebarIcon>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { OfficeBuildingIcon, CogIcon } from '@heroicons/vue/outline';
+import {
+  OfficeBuildingIcon,
+  CogIcon,
+  BeakerIcon,
+  CalculatorIcon,
+  ChartBarIcon,
+  UserIcon
+} from '@heroicons/vue/outline';
 </script>
