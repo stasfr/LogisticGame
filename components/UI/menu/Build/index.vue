72adf4e1d5591bd6fc92c9a01a2785d92da30ddc
stasfr <stas240600@yandex.ru>
2023-07-06 23:31:08 +0300

    tailwind scrollbar

--- components/UI/menu/Build/index.vue
+++ components/UI/menu/Build/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex max-h-[90%] flex-wrap justify-around overflow-hidden overflow-y-scroll px-10"
+    class="flex max-h-[90%] flex-wrap justify-around overflow-hidden overflow-y-scroll px-10 scrollbar-thin scrollbar-track-slate-950 scrollbar-thumb-slate-700"
   >
     <UIMenuBuildItem
       v-for="buildMenuItem in state.buildMenuItems"
