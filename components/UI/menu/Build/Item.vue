3d4c9b31d31e3b0cf76982a6b15782c3ae559aa3
stasfr <stas240600@yandex.ru>
2023-07-06 23:40:17 +0300

    unused line

--- components/UI/menu/Build/Item.vue
+++ components/UI/menu/Build/Item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mx-5 mb-10 h-72 shrink-0 basis-48 rounded-xl bg-slate-700">
+  <div class="mx-5 mb-10 h-72 basis-48 rounded-xl bg-slate-700">
     <div class="text-white">{{ data.name }}</div>
   </div>
 </template>
