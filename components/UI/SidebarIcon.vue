6368c2b329267041e9872247c88cb9280f438f4a
stasfr <stas240600@yandex.ru>
2023-07-06 22:02:09 +0300

    delete unused classes

--- components/UI/SidebarIcon.vue
+++ components/UI/SidebarIcon.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="group relative mx-auto mb-2 mt-2 flex h-12 w-12 cursor-pointer items-center justify-center rounded-3xl bg-slate-300 text-xs text-slate-950 transition-all duration-300 hover:rounded-xl hover:bg-slate-800 hover:text-slate-300"
+    class="group relative mx-auto mb-2 mt-2 flex h-12 w-12 cursor-pointer items-center justify-center rounded-3xl bg-slate-300 text-slate-950 transition-all duration-300 hover:rounded-xl hover:bg-slate-800 hover:text-slate-300"
   >
     <div class="h-10 w-10">
       <slot name="icon"></slot>
     </div>
     <span
-      class="absolute left-14 m-2 w-auto min-w-max origin-left scale-0 rounded-md bg-gray-900 p-2 text-xs font-bold text-white shadow-md transition-all duration-100 group-hover:scale-100"
+      class="absolute left-14 m-2 w-auto min-w-max origin-left scale-0 rounded-md bg-gray-900 p-2 font-bold text-white shadow-md transition-all duration-100 group-hover:scale-100"
     >
       <slot name="tooltip"></slot>
     </span>
